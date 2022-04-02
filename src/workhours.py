0b2ef16eb87da3ac541ee582cb848ac5db32e21e
care1e55 <sfilms89@gmail.com>
2022-04-02 23:18:40 +0300

    add: workhours template

--- src/workhours.py
+++ src/workhours.py
@@ -3,13 +3,16 @@ from datetime import datetime, timedelta
 from typing import Iterable
 
 
-class Weekend(Enum):
-    SATURDAY = 5
-    SUNDAY = 6
-
+# class Weekend(Enum):
+#     SATURDAY = 5
+#     SUNDAY = 6
+#
 
 SECONDS_IN_HOUR = 60 * 60
-WEEKENDS = (Weekend.SATURDAY, Weekend.SUNDAY)
+HOURS_IN_DAY = 24
+WEEKENDS = (5, 6)
+WORK_START = datetime(hour=9)
+WORK_END = datetime(hour=17)
 
 
 def to_datetime(date: str) -> datetime:
@@ -18,11 +21,11 @@ def to_datetime(date: str) -> datetime:
 
 def dates(date_from: datetime, date_to: datetime) -> Iterable:
     delta = date_to - date_from
-    for days in range(delta.days):
-        yield date_from + timedelta((date_to - date_from).days)
+    for days in range(delta.days + 1):
+        yield date_from + timedelta(days)
 
 
-def count_weekends(date_from: datetime, date_to: datetime) -> int:
+def weekends(date_from: datetime, date_to: datetime) -> int:
     weekend_counter = 0
     for date in dates(date_from, date_to):
         if date.weekday() in WEEKENDS:
@@ -30,8 +33,6 @@ def count_weekends(date_from: datetime, date_to: datetime) -> int:
     return weekend_counter
 
 
-def count_workhours(date_from: str, date_to: str) -> int:
+def workhours(date_from: str, date_to: str) -> int:
     date_from, date_to = to_datetime(date_from), to_datetime(date_to)
-    weekends_count = count_weekends(date_to - date_from)
-    workhours = (date_to - date_from).total_seconds() // SECONDS_IN_HOUR - weekends_count
-    return int(workhours)
+    return int((date_to - date_from).total_seconds() // SECONDS_IN_HOUR) - weekends(date_to, date_from) * HOURS_IN_DAY
