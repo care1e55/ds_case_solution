4a22602087d6e40f6d08a3ae2f56e44a8d546e59
care1e55 <sfilms89@gmail.com>
2022-04-04 00:50:46 +0300

    add: encoding

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+
+import numpy as np
 import typer
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
 import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
 
 app = typer.Typer()
 
@@ -12,8 +17,47 @@ class Pipeline:
     def __init__(self, *args, **kwargs):
         self.model = GradientBoostingRegressor(*args, **kwargs)
 
+    @staticmethod
+    def encode_cyclic(data: pd.DataFrame, column: str) -> pd.Series:
+        max_x = data[column].max()
+        return data[column].apply(
+            lambda x: np.cos(2 * np.pi * x / (max_x + 1))
+        ).reset_index(drop=True)
+
+    @staticmethod
+    def encode_ordinal(data: pd.DataFrame, column: str) -> pd.DataFrame:
+        return pd.DataFrame(OrdinalEncoder().fit_transform(
+            data[column].values.reshape(data.shape[0], 1)
+        ).astype(int))
+
+    @staticmethod
+    def encode_binary_onehot(data: pd.DataFrame, column: str) -> pd.DataFrame:
+        return pd.get_dummies(data[column]).rename(columns={0: f'not_{column}', 1: column}).reset_index(drop=True)
+
     def _get_features(self, data):
-        return data.drop(columns=['dteday', 'cnt'])
+        features = data[[
+            'yr',
+            'mnth',
+            'weekday',
+            'workingday',
+            'holiday',
+            'hum',
+            'temp',
+            'windspeed',
+        ]]
+        encoded_weekdays = self.encode_cyclic(features, 'weekday')
+        encoded_holiday = self.encode_binary_onehot(features, 'holiday')
+        encoded_workingday = self.encode_binary_onehot(features, 'workingday')
+        encoded_mnth = self.encode_ordinal(features, 'mnth')
+        features = pd.concat(
+            (
+                 features.drop(['holiday', 'workingday', 'mnth', 'weekday'], axis=1).reset_index(drop=True),
+                 encoded_weekdays,
+                 encoded_holiday,
+                 encoded_workingday,
+                 encoded_mnth
+            ), axis=1)
+        return features
 
     def _get_target(self, data):
         return data['cnt']
